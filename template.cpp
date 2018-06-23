411f481595f1f31e7a4fcf444fef57febe4a2a70
Tamilarasu S <tamilarasuiit@gmail.com>
2018-06-23 22:32:19 +0530

    Initial Repository

---
+++ template.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <utility>
+#include <ctime>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+
+using namespace std;
+
+typedef long long int ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<vi> vvi;
+typedef pair<int, int> pii;
+typedef vector<pii> vpii;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+
+
+	return 0;
+}
