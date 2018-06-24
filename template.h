a5fd6ccefbd19965494d26ba22104de3ba01b08d
Tamilarasu S <tamilarasuiit@gmail.com>
2018-06-24 16:37:57 +0530

    Add Template header

---
+++ template.h
@@ -0,0 +1,26 @@
+#ifndef TEMPLATE
+#define TEMPLATE
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <utility>
+#include <ctime>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+
+using namespace std;
+
+typedef long long int ll;
+typedef vector<int> vi;
+typedef vector<bool> vb;
+typedef vector<ll> vll;
+typedef vector<vi> vvi;
+typedef pair<int, int> pii;
+typedef vector<pii> vpii;
+
+#endif
