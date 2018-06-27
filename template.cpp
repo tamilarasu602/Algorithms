32d0ddee82578c222736737de77efa6fe2dcdd76
Tamilarasu S <tamilarasuiit@gmail.com>
2018-06-27 13:40:31 +0530

    Update template to include max, min macros

--- template.cpp
+++ template.cpp
@@ -20,6 +20,12 @@ typedef vector<vi> vvi;
 typedef pair<int, int> pii;
 typedef vector<pii> vpii;
 
+// utility functions
+#define ARGMAX(a, b, c, d) (((a) > (b)) ? (c) : (d))
+#define ARGMIN(a, b, c, d) (((a) > (b)) ? (c) : (d))
+#define VMAX(a, b) ARGMAX(a, b, a, b)
+#define VMIN(a, b) ARGMIN(a, b, a, b)
+
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
